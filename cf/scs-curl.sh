ac6445f86ac4a3eacdcaccec37b962489d404fea
Scott Frederick <scottyfred@gmail.com>
2017-09-21 10:54:55 -0500

    Remove obsolete bosh scripts.

--- cf/scs-curl.sh
+++ cf/scs-curl.sh
@@ -9,8 +9,6 @@
 #     $ cf service-key config-server config-server-key | scs-curl.sh "/encrypt/status"
 #     $ cf service-key config-server config-server-key | scs-curl.sh "/encrypt" -d "some text to encrypt"
 
-set -o xtrace
-
 CREDENTIALS="`cat | sed 1,2d`"
 
 if [[ "$CREDENTIALS" == "" ]]; then
@@ -19,10 +17,19 @@ if [[ "$CREDENTIALS" == "" ]]; then
 fi
 
 URI=$(echo $CREDENTIALS | jq -r .uri)
-TOKEN_URI=$(echo $CREDENTIALS | jq -r .access_token_uri)
-CLIENT_ID=$(echo $CREDENTIALS | jq -r .client_id)
-CLIENT_SECRET=$(echo $CREDENTIALS | jq -r .client_secret)
 
-TOKEN=$(curl -k -s $TOKEN_URI -u $CLIENT_ID:$CLIENT_SECRET -d grant_type=client_credentials | jq -r .access_token)
+# TOKEN_URI=$(echo $CREDENTIALS | jq -r .access_token_uri)
+# CLIENT_ID=$(echo $CREDENTIALS | jq -r .client_id)
+# CLIENT_SECRET=$(echo $CREDENTIALS | jq -r .client_secret)
+#
+# TOKEN=$(curl -k -s $TOKEN_URI -u $CLIENT_ID:$CLIENT_SECRET -d grant_type=client_credentials | jq -r .access_token)
+#
+# curl -k -s -H "Authorization: bearer $TOKEN" $URI"$@"
 
-curl -k -s -H "Authorization: bearer $TOKEN" $URI"$@"
+CONFIG_HOME=~/.cf
+
+AUTH=$(cat $CONFIG_HOME/config.json | jq -r .AccessToken)
+
+echo "AUTH=$AUTH"
+
+curl -k -s -H "Authorization: $AUTH" $URI"$@"
