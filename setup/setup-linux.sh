d2208237c2210b508820b0454bfb69e796110baf
Scott Frederick <sfrederick@vmware.com>
2021-11-09 15:23:00 -0600

    Update setup script

--- setup/setup-linux.sh
+++ setup/setup-linux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 echo "Updating Linux"
 sudo apt update
 sudo apt full-upgrade -y
@@ -8,7 +10,7 @@ sudo apt install build-essential apt-transport-https curl file
 echo ""
 echo "Installing zsh"
 sudo apt install zsh
-chsh /bin/zsh
+chsh --shell /usr/bin/zsh
 
 echo ""
 echo "Installing git"
@@ -18,26 +20,29 @@ echo ""
 echo "Installing vim"
 sudo apt install vim
 
+if [ ! -d .scripts ]; then
+  echo ""
+  echo "Cloning scripts"
+  git clone https://github.com/scottfrederick/scripts .scripts
+fi
 
-echo ""
-echo "Cloning scripts"
-git clone https://github.com/scottfrederick/scripts .scripts
+if [ ! -d .dotfiles ]; then
+  echo ""
+  echo "Cloning dotfiles"
+  git clone https://github.com/scottfrederick/dotfiles .dotfiles
+  cd .dotfiles
+  ./setup-symlinks.sh
+  cd -
+fi
 
-
-echo ""
-echo "Cloning dotfiles"
-git clone https://github.com/scottfrederick/dotfiles .dotfiles
-cd .dotfiles
-./setup-symlinks.#!/bin/sh
-cd -
-source .zshrc
-
-
-echo ""
-echo "Installing oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
+if [ ! -d ${HOME}/.oh-my-zsh ]; then
+  echo ""
+  echo "Installing oh-my-zsh"
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  mkdir -p $HOME/.dotfiles/zsh/custom/plugins
+  git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.dotfiles/zsh/custom/plugins/zsh-autosuggestions
+fi
 
 echo ""
 echo "Start a new shell"
+
