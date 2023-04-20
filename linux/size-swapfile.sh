f49e3cb81386becef3601c295fd474ec0db2d990
Scott Frederick <sfrederick@vmware.com>
2023-04-20 09:15:32 -0500

    Update swap file size

--- linux/size-swapfile.sh
+++ linux/size-swapfile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo swapoff /swapfile
-sudo dd if=/dev/zero of=/swapfile count=4 bs=1G
+sudo dd if=/dev/zero of=/swapfile count=16 bs=1G
 sudo mkswap /swapfile
 sudo chmod 0600 /swapfile
 sudo swapon /swapfile
